93d9ad7a27b65e1da928fdaffd6064f1b95193c8
Piotr Mazurek <Piotr Mazurek>
2017-01-02 14:44:45 +0100

    commit

--- handshake.int/app/Http/Controllers/Auth/RegisterController.php
+++ handshake.int/app/Http/Controllers/Auth/RegisterController.php
@@ -39,6 +39,18 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        $this->guard()->login($user);
+
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
+    }
+
     /**
      * Get a validator for an incoming registration request.
      *
