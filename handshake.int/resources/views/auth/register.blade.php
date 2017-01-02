0f1eb110c551047eb233815753955b16eb8f2be1
Piotr Mazurek <Piotr Mazurek>
2017-01-02 16:24:20 +0100

    webcam foto pakken

--- handshake.int/resources/views/auth/register.blade.php
+++ handshake.int/resources/views/auth/register.blade.php
@@ -5,51 +5,17 @@
 <div class="section">
   <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
       {{ csrf_field() }}
-
-      <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-          <label for="name" class="col-md-4 control-label">Name</label>
-
-          <div class="col-md-6">
-              <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-              @if ($errors->has('name'))
-                  <span class="help-block">
-                      <strong>{{ $errors->first('name') }}</strong>
-                  </span>
-              @endif
-          </div>
-      </div>
-
-      <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-          <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-          <div class="col-md-6">
-              <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-              @if ($errors->has('email'))
-                  <span class="help-block">
-                      <strong>{{ $errors->first('email') }}</strong>
-                  </span>
-              @endif
-          </div>
-      </div>
-
-      <div class="form-group">
-          <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-          <div class="col-md-6">
-              <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-          </div>
-      </div>
-
-      <div class="form-group">
-          <div class="col-md-6 col-md-offset-4">
-              <button type="submit" class="btn btn-primary">
-                  Register
-              </button>
-          </div>
-      </div>
   </form>
+    <div class="camera">
+        <video id="video">Video stream not available.</video>
+        <button id="startbutton">Take photo</button>
+    </div>
+    <canvas id="canvas">
+    </canvas>
+    <div class="output">
+        <img id="photo" alt="The screen capture will appear in this box.">
+    </div>
+    <script src="/js/photocapture.js"></script>
 </div>
 
 
