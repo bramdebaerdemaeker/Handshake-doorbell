5f25450da5b68e5729fa49a80d7ca514d5c74cb1
Bram De Baerdemaeker <bram.debaerdemaeker@gmail.com>
2017-01-15 22:27:02 +0100

    auth

--- handshake.int/resources/views/auth/login.blade.php
+++ handshake.int/resources/views/auth/login.blade.php
@@ -1,29 +1,8 @@
 @extends('layout')
 
 @section('content')
-<!--
-<div class="section">
-  <div class="section-login">
 
-    <div class="instructions">
-      <h2>Face recognition</h2>
-      <hr>
-      <ul>
-        <li>data</li>
-        <li>data</li>
-        <li>data</li>
-        <li>data</li>
-      </ul>
-    </div>
-    <div class="picture">
-      <img src="http://placehold.it/200x200">
-      <button type="button" name="button">take picture</button>
-    </div>
-
-  </div>
-</div> -->
-
-<div class="section">
+<!-- <div class="section">
   <form method="POST" action="{{ url('/login') }}" enctype="multipart/form-data">
       {{ csrf_field() }}
   <div class="picture">
@@ -36,9 +15,40 @@
   </div>
       <input type="hidden" id="photo" name="photo">
   </form>
-</div>
+</div> -->
 
 
+
+<div class="section--authorization">
+
+    <div class="section--authorization__brand">
+      <div class="section--authorization__brand__img">
+      </div>
+      <div class="section--authorization__brand__title">
+          <a href="/"><h1>Handshake</h1></a>
+      </div>
+    </div>
+
+    <div class="section-auth--register">
+      <form method="POST" action="{{ url('/login') }}" enctype="multipart/form-data">
+        {{ csrf_field() }}
+
+        <!-- face recognition -->
+          <div class="section-auth__picture">
+            <h2>Show us your beautiful smile</h2>
+            <div class="camera">
+                <video id="video" width="300" height="300">Video stream not available.</video>
+            </div>
+            <canvas id="canvas"></canvas>
+            <button type="submit" name="button" class="hide-picture" id="startbutton"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></button>
+          </div>
+
+        </form>
+
+        <div class="section-auth__login">
+          <a href="/login">Not a member yet? <span>Sign up!</span></a>
+        </div>
+    </div>
 
 </div>
 
