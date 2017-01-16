fb4c179e2bd0d39ffc7f88218e96b7c74f9315c6
Bram De Baerdemaeker <bram.debaerdemaeker@gmail.com>
2017-01-16 01:03:29 +0100

    e,q

--- handshake.int/resources/views/auth/login.blade.php
+++ handshake.int/resources/views/auth/login.blade.php
@@ -2,22 +2,6 @@
 
 @section('content')
 
-<!-- <div class="section">
-  <form method="POST" action="{{ url('/login') }}" enctype="multipart/form-data">
-      {{ csrf_field() }}
-  <div class="picture">
-    <h2>Face recognition</h2>
-    <div class="camera">
-      <video id="video">Video stream not available.</video>
-    </div>
-    <canvas id="canvas"></canvas>
-    <button type="submit" name="button" class="hide-picture" id="startbutton">take picture</button>
-  </div>
-      <input type="hidden" id="photo" name="photo">
-  </form>
-</div> -->
-
-
 
 <div class="section--authorization">
 
